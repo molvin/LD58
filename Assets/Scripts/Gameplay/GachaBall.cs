3bf3ea5e9100c7db6ce2fbbe62ca96f17e6b1e08
Eretol <morrismertner@gmail.com>
2025-10-06 20:44:52 +0200

    balls

--- Assets/Scripts/Gameplay/GachaBall.cs
+++ Assets/Scripts/Gameplay/GachaBall.cs
@@ -5,22 +5,42 @@ public class GachaBall : MonoBehaviour
     public Pawn Prefab;
     public PawnRarity Rarity;
     public SphereCollider ClickCollider;
+    public Rigidbody BallBody;
     public AudioEvent BonkNoise;
     public AudioEvent Rolling;
 
-    private bool doSound;
+    private bool doSound = true;
     public AudioPlayer playah;
 
     public async void Start()
     {
-        await Awaitable.WaitForSecondsAsync(0.1f);
-        if (BonkNoise != null)
-            AudioManager.Play(BonkNoise, this.transform.position);
+//        await Awaitable.WaitForSecondsAsync(0.1f);
+  //      if (BonkNoise != null)
+    //        AudioManager.Play(BonkNoise, this.transform.position);
         await Awaitable.WaitForSecondsAsync(0.1f);
         if (Rolling != null)
             playah = AudioManager.Play(Rolling, this.transform.position);
 
-        
+        SetTimerForBody();
+    }
+
+    public void Update()
+    {
+        Debug.Log("Angular X : " + BallBody.angularVelocity.x);
+        Debug.Log("Angular Y : " + BallBody.angularVelocity.y);
+        Debug.Log("Angular X : " + BallBody.linearVelocity.x);
+        Debug.Log("Angular Y : " + BallBody.linearVelocity.y);
+        if (BallBody.angularVelocity.x == 0f && doSound == false)
+        {
+            playah.Source.Stop();
+        }
+    }
+
+    public async void SetTimerForBody()
+    {
+        await Awaitable.WaitForSecondsAsync(2f);
+        doSound = false;
+
     }
 
     public void OnCollisionEnter(Collision collision)
@@ -30,5 +50,7 @@ public class GachaBall : MonoBehaviour
             if (BonkNoise != null)
                 AudioManager.Play(BonkNoise, this.transform.position);
         }
+
     }
+
 }
